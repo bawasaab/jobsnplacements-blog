0d737926e408aed570d064d63c7cabb3eee6e476
Deepak Bawa <bawa_d@ymail.com>
2023-06-04 17:20:09 +0530

    added layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,37 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     {
-        path: 'home',
-        loadComponent: () => import('./home/home.component').then((x) => x.HomeComponent),
-    },
-    {
-        path: 'about-us',
-        loadComponent: () => import('./about-us/about-us.component').then((x) => x.AboutUsComponent),
-    },
-    {
-        path: 'privacy-policy',
-        loadComponent: () => import('./privacy-policy/privacy-policy.component').then((x) => x.PrivacyPolicyComponent),
-    },
-    {
-        path: 'contact-us',
-        loadComponent: () => import('./contact-us/contact-us.component').then((x) => x.ContactUsComponent),
-    },
-    {
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'home',
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'home',
+                loadComponent: () => import('./home/home.component').then((x) => x.HomeComponent),
+            },
+            {
+                path: 'about-us',
+                loadComponent: () => import('./about-us/about-us.component').then((x) => x.AboutUsComponent),
+            },
+            {
+                path: 'privacy-policy',
+                loadComponent: () => import('./privacy-policy/privacy-policy.component').then((x) => x.PrivacyPolicyComponent),
+            },
+            {
+                path: 'contact-us',
+                loadComponent: () => import('./contact-us/contact-us.component').then((x) => x.ContactUsComponent),
+            },
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'home',
+            },
+            {
+                path: '**',
+                component: NotFoundComponent
+            }
+        ]
     }
 ];
